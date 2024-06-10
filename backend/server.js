d3b68f4d612499e72558d5f882a7e47636e7248c
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2024-06-10 18:23:32 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,5 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server is running ${HOST}/${PORT}`);
+  console.log(`Server is running ${HOST}:${PORT}`);
 });
